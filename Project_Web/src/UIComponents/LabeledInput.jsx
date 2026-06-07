44dedcd5045ba9136ccf6f5f8bd5f2b2c48c2103
YanivR13 <132251678+YanivR13@users.noreply.github.com>
2026-06-07 19:28:28 +0300

    Delete Project_Web directory
    NO NEED OF Project_Web directory in the branch

--- Project_Web/src/UIComponents/LabeledInput.jsx
+++
@@ -1,8 +0,0 @@
-export default function LabeledInput(props) {
-    return (
-      <div>
-        <label className="block text-sm font-medium">{props.label}</label>
-        <input className="w-full p-3 mb-4 border rounded" type={props.type} disabled={props.disabled} value={props.value} onChange={props.onChange} />
-      </div>
-    );
-}
